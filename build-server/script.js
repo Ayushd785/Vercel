f28937d5375282ed01b51abdde2ab9ee9ab8a881
Ayushd785 <ayushd785@gmail.com>
2025-08-29 20:24:07 +0530

    wrote the basic upload to s3 logic

--- build-server/script.js
+++ build-server/script.js
@@ -1,7 +1,17 @@
 const { exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
+const { PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 
+const mime = require("mime-types");
+
+const s3Client = new S3Client({
+  region: "",
+  credentials: {
+    accessKeyId: "",
+    secretAccessKey: "",
+  },
+});
 async function init() {
   console.log("Executing script.js");
   const outDirPath = path.join(__dirname, "output");
@@ -16,7 +26,7 @@ async function init() {
     console.log("error", data.toSting());
   });
 
-  p.on("close", function () {
+  p.on("close", async function () {
     console.log("build complete");
     const distFolderPath = path.join(__dirname, "output", "dist");
     const distFolderContents = fs.readdirSync(distFolderPath, {
@@ -25,7 +35,16 @@ async function init() {
 
     for (const filePath of distFolderContents) {
       if (fs.lstatSync(filePath).isDirectory) continue;
+
+      const command = new PutObjectCommand({
+        Bucket: "",
+        Key: `__outputs/${PROJECT_ID}/${filePath}`,
+        Body: fs.createReadStream(filePath),
+        ContentType: mime.lookup(filePath),
+      });
+      await s3Client.send(command);
     }
+    console.log("Done .....");
   });
 }
 
